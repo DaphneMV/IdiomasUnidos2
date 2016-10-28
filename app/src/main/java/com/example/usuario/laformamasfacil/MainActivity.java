247495f4893d875b89f05c2407cd04673e3d7c31
Usuario <dap_selena@hotmail.com>
2016-10-28 11:35:39 -0300

    VentanaInicio

---
+++ app/src/main/java/com/example/usuario/laformamasfacil/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.usuario.laformamasfacil;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.RadioGroup;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RadioGroup r=
+    }
+}
---
+++ app/src/main/java/com/example/usuario/laformamasfacil/SplashScreen.java
@@ -0,0 +1,22 @@
+package com.example.usuario.laformamasfacil;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent a=new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(a);
+            }
+        },4000);
+    }
+}
